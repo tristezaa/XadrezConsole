d5a7f7ef1a379ae0a3031562a2c74827e4d22b99
Renata de Freitas <renatafreitas2c@gmail.com>
2026-03-30 15:22:03 -0300

    Criando a classe PositionChess

---
+++ XadrezConsole/chess/PositionChess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XadrezConsole.board;
+
+namespace XadrezConsole.chess
+{
+    internal class PositionChess
+    {
+        public char column { get; set; }
+        public int line { get; set; }
+
+        public PositionChess(char column, int line)
+        {
+            this.column = column;
+            this.line = line;
+        }
+
+        public Position toPosition ()
+        {
+            return new Position(8 - line, column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + column + line;
+        }
+    }
+}
